49d27961f5916c8fb03bc9f3192a323e04537027
Rhymedys_ <rhymedys@foxmail.com>
2019-08-04 20:47:05 +0800

    add systemctl service script

---
+++ stop.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export PROJECT_PATH=/usr/local/web-server/test-proxy
+
+export PATH=/root/.tnvm/versions/alinode/v4.7.0/bin:/usr/node/bin:$PATH
+
+echo "node version"
+	node -v
+echo "npm version" 
+	npm -v
+
+stop(){
+	cd $PROJECT_PATH
+	npm run stop
+}
+
+stop
+
+
+
