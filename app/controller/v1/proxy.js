410a8484d8da8f629e27640b1dd1d089ef319c51
rhymedys <rhymedys@gmail.com>
2019-11-11 14:49:18 +0800

    新增动态传token

--- app/controller/v1/proxy.js
+++ app/controller/v1/proxy.js
@@ -2,7 +2,7 @@
  * @Author: Rhymedys/Rhymedys@gmail.com
  * @Date: 2018-07-27 10:35:34
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2019-11-11 09:55:56
+ * @Last Modified time: 2019-11-11 10:03:55
  */
 
 'use strict';
